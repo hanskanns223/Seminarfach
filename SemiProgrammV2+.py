1f1916819d7a83b69b5cc562a32078194b09b148
SoereW <soeren.wolleschensky@outlook.de>
2024-03-01 23:02:19 +0100

    Grundlagen

--- SemiProgrammV2+.py
+++ SemiProgrammV2+.py
@@ -8,9 +8,9 @@ global RGBdBild # Durchschnittlicher RGB über alle Bilder
 def Ordnerauslesen(Bildpfad):
     bilder_liste = glob.glob(os.path.join(Bildpfad, '*.jpg'))
     print(f'Es wurden {str(len(bilder_liste))} Bilder in dem Ordner gefunden')
-    ydalleBild = HelligkeitjedesBilder(bilder_liste)
-    print(f'Durchschnittliche Helligkeit aller Bilder: {ydalleBild}')
-
+    #ydalleBild = HelligkeitjedesBilder(bilder_liste)
+    #print(f'Durchschnittliche Helligkeit aller Bilder: {ydalleBild}')
+    Bilderzusammenfügen(bilder_liste, Image.open(bilder_liste[0]).size)
 def HelligkeitjedesBilder(ListeBilder):
     for Bild in ListeBilder:
         img = Image.open(Bild)
@@ -18,18 +18,32 @@ def HelligkeitjedesBilder(ListeBilder):
         RGBdBild = np.mean(data, axis=(0, 1))
         print(RGBdBild)
         # Rote Werte
-        data[:, :, 0] = np.where(data[:, :, 0] < RGBdBild[0], data[:, :, 0] - (data[:, :, 0] - RGBdBild[0]) ** 2,
+        data[:, :, 0] = np.where(data[:, :, 0] < RGBdBild[0], data[:, :, 0] - (data[:, :, 0] - RGBdBild[0]) ,
                                  np.where(data[:, :, 0] > RGBdBild[0],
-                                          data[:, :, 0] + (data[:, :, 0] - RGBdBild[0]) ** 2, data[:, :, 0]))
+                                          data[:, :, 0] + (data[:, :, 0] - RGBdBild[0]), data[:, :, 0]))
 
         # Grüne Werte
-        data[:, :, 1] = np.where(data[:, :, 1] < RGBdBild[1], data[:, :, 1] - (data[:, :, 1] - RGBdBild[1]) ** 2,
+        data[:, :, 1] = np.where(data[:, :, 1] < RGBdBild[1], data[:, :, 1] - (data[:, :, 1] - RGBdBild[1]),
                                  np.where(data[:, :, 1] > RGBdBild[1],
-                                          data[:, :, 1] + (data[:, :, 1] - RGBdBild[1]) ** 2, data[:, :, 1]))
+                                          data[:, :, 1] + (data[:, :, 1] - RGBdBild[1]), data[:, :, 1]))
 
         # Blaue Werte
-        data[:, :, 2] = np.where(data[:, :, 2] < RGBdBild[2], data[:, :, 2] - (data[:, :, 2] - RGBdBild[2]) ** 2,
+        data[:, :, 2] = np.where(data[:, :, 2] < RGBdBild[2], data[:, :, 2] - (data[:, :, 2] - RGBdBild[2]),
                                  np.where(data[:, :, 2] > RGBdBild[2],
-                                          data[:, :, 2] + (data[:, :, 2] - RGBdBild[2]) ** 2, data[:, :, 2]))
+                                          data[:, :, 2] + (data[:, :, 2] - RGBdBild[2]), data[:, :, 2]))
         Image.fromarray(data.astype(np.uint8)).save("extrahierte_bilder/manipuliertes_bild.jpg")
+def Bilderzusammenfügen(ListeBilder, imgsize):
+    i = 0
+    endbild = np.zeros((imgsize[1], imgsize[0], 3), dtype=np.uint8)
+    for Bild in ListeBilder:
+        img = Image.open(Bild)
+        data = np.array(img)
+
+        if i == 0:
+            endbild = data[:,:]
+        else:
+            endbild = (endbild[:,:] + data[:,:]) // 2
+            print(endbild[0][0][0])
+        i +=1
+    Image.fromarray(endbild.astype(np.uint8)).save("Ausgansbild.jpg")
 Ordnerauslesen('extrahierte_bilder')
